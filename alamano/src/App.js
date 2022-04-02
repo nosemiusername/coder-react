6ca906b847a0fe21df8e0b30684df9564dd81a02
nosemiusername <rafael@imatec.cl>
2022-04-02 13:25:07 -0300

    fixing footer

--- alamano/src/App.js
+++ alamano/src/App.js
@@ -5,28 +5,34 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import NotFound from './pages/NotFound';
+import ContactUs from './pages/ContactUs';
+import History from './pages/History'
+import JoinUs from './pages/JoinUs'
 
 function App() {
 
   return (
-    <>
-      <BrowserRouter>
+    <BrowserRouter>
+      <div className='content'>
         <Navbar />
-        {/* <ItemListContainer /> */}
         <Routes>
           <Route path="/" element={<ItemListContainer />} />
           <Route path="/category/:category" element={<ItemListContainer />} />
           <Route path="/item/:id" element={<ItemDetailContainer />} />
           <Route path="/*" element={<NotFound />} />
+          <Route path="/*" element={<NotFound />} />
+          <Route path="/contactanos" element={<ContactUs />} />
+          <Route path="/historia" element={<History />} />
+          <Route path="/unete" element={<JoinUs />} />
           {/* <Route path="/cart" element={<CartWidget />} />
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/profile" element={<Profile />} /> */}
         </Routes>
-        <Footer />
-      </BrowserRouter>
-    </>
+      </div>
+      <Footer />
+    </BrowserRouter>
 
   );
 }
---
+++ alamano/src/pages/ContactUs.js
@@ -0,0 +1,11 @@
+import Container from "@mui/material/Container";
+
+function ContactUs() {
+    return (
+        <Container>
+            <h1>Contáctanos</h1>
+        </Container>
+    )
+}
+
+export default ContactUs;
