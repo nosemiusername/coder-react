e58b8608fcefa4c41a961d6df3d83c9db4e76467
nosemiusername <rafael@imatec.cl>
2022-04-03 12:23:34 -0400

    fixing detail

--- alamano/src/components/ItemDetail/ItemDetail.js
+++ alamano/src/components/ItemDetail/ItemDetail.js
@@ -1,26 +1,25 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
+import CardHeader from '@mui/material/CardHeader';
 import Typography from '@mui/material/Typography';
 import ItemCount from '../ItemCount/ItemCount'
 import { Grid } from '@mui/material';
-
+import './ItemDetail.css';
 const ItemDetail = ({ detail }) => {
     const countSetting = { initial: 1, stock: detail.stock };
     return (
 
-        <Grid container>
+        <Grid
+            container
+            justifyContent="space-around"
+            spacing={6}
+        >
             <Grid item xs={6}>
+                <img className="grid-img" src={detail.pictureUrl} alt={detail.name} />
             </Grid>
             <Grid item xs={6}>
                 <Card sx={{ maxWidth: 345 }}>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image={detail.pictureUrl}
-                        alt={detail.title}
-                    />
                     <CardContent>
                         <Typography sx={{ color: '#353535' }} gutterBottom variant="h5" component="div">
                             {detail.title}
@@ -31,11 +30,31 @@ const ItemDetail = ({ detail }) => {
                         <Typography variant="body2" color="textSecondary" component="div">
                             {detail.description}
                         </Typography>
-                        <Typography style={{ fontWeight: 600 }}>
+                        <Typography style={{ fontWeight: 800, color: '#F43D53' }}>
                             ${detail.price}
                         </Typography>
                     </CardContent>
                     <ItemCount {...countSetting} />
+                    <CardContent>
+                        <Typography sx={{ color: '#353535' }} gutterBottom variant="h6" component="div">
+                            Detalles
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="div">
+                            DIMENSIONES: {detail.size}
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="div">
+                            COLOR: {detail.color}
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="div">
+                            MATERIAL: {detail.material}
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="div">
+                            DISEÑADO POR: {detail.author}
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="div">
+                            CODIGO: {detail.code}
+                        </Typography>
+                    </CardContent>
                 </Card>
             </Grid>
         </Grid>
